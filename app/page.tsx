509ebc2c3101d15b49ecc3dab9f556e1060500ba
Sharathxct <sharathxct@gmail.com>
2025-03-07 13:57:22 +0530

    exp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { headers } from "next/headers";
+
 export default function Home() {
-  console.log("hello");
+  const ipAddress = headers().then((headers) => headers.get("x-forwarded-for"));
+
   return (
     <div className="flex flex-col items-center justify-center">
       <input type="name" autoComplete="email" autoFocus />
-      <h1>Hello World</h1>
+      <h1>hi {ipAddress || 'unknown'}</h1>
     </div>
   );
 }
+
