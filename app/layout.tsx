75cb22be1e2c00f08b8c52d0f90aae73bc51bd24
Sharathxct <sharathxct@gmail.com>
2025-03-07 13:36:05 +0530

    remove analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,6 @@ export default function RootLayout({
       >
         {children}
       </body>
-
-      <script>{`
-(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('lYNOR8xrG0O4WQJZ');
-`}</script>
-
     </html>
   );
 }
